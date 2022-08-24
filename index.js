7db80d10a4986de0bcb774a84891caf160de1224
dessa995 <dessa995@outlook.com>
2022-08-24 15:28:35 +0200

    working wersion

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
-  $(".btn1").click(function () {
-    $(".btn1-div").toggleClass(".btn1-div-visable");
+  $(".btn1").hover(function () {
+    $(".btn1-div").toggleClass("btn1-div-visable");
   });
 
-  $(".btn1").click(function () {
-    var id = this.id;
-    var split_id = id.split("_");
-    var post_id = "post_" + split_id[1];
+  $(".btn2").click(function () {
+    $(".btn2-div").toggleClass("btn2-div-visable");
+  });
+
+  $(".btn3").hover(function () {
+    $(".btn3-div").toggleClass("btn3-div-visable");
+  });
+
+  $(".btn4").click(function () {
+    $(".btn4-div").toggleClass("btn4-div-visable");
   });
 });
